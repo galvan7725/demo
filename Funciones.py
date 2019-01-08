0d8ed5f609e95f2437d2e9dcaae6782c3736dcc7
LuisGalvan <galvan7725@gmail.com>
2019-01-07 21:05:53 -0600

    Inicial

---
+++ Funciones.py
@@ -0,0 +1,22 @@
+print("Funciones en python")
+
+def mensaje():
+    print("Funcion en python")
+
+mensaje()
+
+def mensaje2():
+    print("Funcion numero 2 en python")
+
+mensaje2()
+
+def param(num1, num2):
+    print(num1+num2)
+
+param(1,1)
+
+def ret(n1,n2,n3):
+    return n1+n2+n3
+
+num = ret(1,1,1)
+print("Resultado:",num)
---
+++ Listas.py
@@ -0,0 +1,10 @@
+lista=["Luis","Maria","Cecy","Beto"]
+print(lista[:])
+#añadir elementos a la lista
+lista.append("Isma")
+print(lista[:])
+
+lista.extend(["Gordo","Eve"])
+print(lista[:])
+print(lista.index("Luis"))
+print("Luis" in lista) 
