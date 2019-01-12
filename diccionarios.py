1844009472ffcc47d8b200e2c5f536752e5e61a1
LuisGalvan <galvan7725@gmail.com>
2019-01-12 17:54:13 -0600

    Metodos para diccionarios

--- diccionarios.py
+++ diccionarios.py
@@ -4,3 +4,9 @@ diccionario["Italia"]="Roma"
 print(diccionario)
 del diccionario["Italia"]
 print(diccionario)
+
+dic={"Mexico" : "DF", "AcMilan": 7, 11:"Jugadores"}
+
+print(diccionario.keys())
+print(diccionario.values())
+print(len(diccionario))
