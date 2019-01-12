89151f67f20f0d28c8c9b02aa5780dbc64bc5739
LuisGalvan <galvan7725@gmail.com>
2019-01-12 17:44:09 -0600

    Diccionario con un solo tipo de datos

---
+++ diccionarios.py
@@ -0,0 +1,6 @@
+diccionario={"Alemania":"Berlin","Francia":"Madrid","Mexico":"DF"}
+print(diccionario)
+diccionario["Italia"]="Roma"
+print(diccionario)
+del diccionario["Italia"]
+print(diccionario)
