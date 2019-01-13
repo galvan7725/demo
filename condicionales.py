eeb25e362aa92811fc5bdfad1b4a9d17e91fc90b
LuisGalvan <galvan7725@gmail.com>
2019-01-12 18:43:18 -0600

    condicional if

---
+++ condicionales.py
@@ -0,0 +1,11 @@
+print("Evaluacion de notas de alumnos")
+
+def evaluacion(nota):
+    valoracion="Aprobado"
+    if nota<5:
+        valoracion="Reprobado"
+    return valoracion
+
+nota1 = input()
+
+print(evaluacion(int(nota1)))
